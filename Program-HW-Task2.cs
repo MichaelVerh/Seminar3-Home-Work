49a65be017e7393ea0780058f87f1cc22804a424
MichaelVerh <107642304+MichaelVerh@users.noreply.github.com>
2022-07-20 23:09:40 +0300

    Add files via upload

---
+++ Program-HW-Task2.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите первую координату первого числа");
+int AX=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую координату первого числа");
+int AY=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью координату первого числа");
+int AZ=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первую координату второго числа");
+int BX=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую координату второго числа");
+int BY=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью координату второго числа");
+int BZ=Convert.ToInt32(Console.ReadLine());
+double distance=Math.Sqrt(Math.Pow(AX-BX, 2)+Math.Pow(AY-BY, 2)+Math.Pow(AZ-BZ, 2));
+Console.WriteLine($"Расстяние между точками в пространстве равно {distance}");
---
+++ Program-HW-Task3.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите чило");
+int Number=Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=Number; i++)
+{
+    Console.WriteLine($"{i}^3={Math.Pow(i,3)}");
+}
